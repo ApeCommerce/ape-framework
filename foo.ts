23d7490c2053b7bb41a270c50deb09cce46b0f0c
Matthieu Symoens <matthieu.symoens@gmail.com>
2023-05-11 16:01:03 -0400

    Packaging

--- foo.ts
+++
@@ -1,9 +0,0 @@
-import { Endpoint } from '@apecommerce/ape-common/endpoint';
-
-const endpoint: Endpoint = {
-  method: 'PATCH',
-  path: '/foo',
-  description: 'The foo bar',
-};
-
-console.log('hello', endpoint);
