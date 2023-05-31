482dbd3ae4bd2c86b7686d99d752cb7f8d4aad7a
Matthieu Symoens <matthieu.symoens@gmail.com>
2023-05-31 12:45:16 -0400

    Unit testing

--- test/unit/boot/getBundle.spec.ts
+++ test/unit/boot/getBundle.spec.ts
@@ -1,14 +1,18 @@
 import 'test/unit/config';
 import { getBundle } from 'boot';
 
-describe('Getting a bundle', () => {
+describe('Getting an existing bundle', () => {
   test('Returns expected value', async () => {
     const fooBundle = await getBundle('foo');
     expect(fooBundle?.bundleId).toBe('foo');
 
     const barBundle = await getBundle('bar');
     expect(barBundle?.bundleId).toBe('bar');
+  });
+});
 
+describe('Getting a missing bundle', () => {
+  test('Fails', async () => {
     const xxxBundle = await getBundle('xxx');
     expect(xxxBundle).toBe(undefined);
   });
