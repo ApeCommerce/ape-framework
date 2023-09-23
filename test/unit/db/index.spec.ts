24c86c0333152adfc11203004a2924c1028cf3fa
Matthieu Symoens <matthieu.symoens@gmail.com>
2023-09-23 12:07:04 -0400

    Updated db module load unit test

--- test/unit/db/index.spec.ts
+++ test/unit/db/index.spec.ts
@@ -4,8 +4,10 @@ import Knex from 'db/knex';
 describe('Loading the module', () => {
   test('Returns an instance of Knex', async () => {
     const db: Knex = (await import('db')).default;
-    expect(typeof db.select).toBe('function');
+    expect(typeof db.raw).toBe('function');
     expect(typeof db.insert).toBe('function');
+    expect(typeof db.select).toBe('function');
     expect(typeof db.update).toBe('function');
+    expect(typeof db.delete).toBe('function');
   });
 });
