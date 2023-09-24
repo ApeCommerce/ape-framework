1810921960c13eaf727ff29edcb999c7f109556f
Matthieu Symoens <matthieu.symoens@gmail.com>
2023-09-24 18:40:23 -0400

    Updated docker compose tests

--- docker-compose-test.sh
+++ docker-compose-test.sh
@@ -1,5 +1,2 @@
-stack=$1
-test=$2
-
-docker compose -f docker-compose.$stack.yml run --rm --quiet-pull node yarn test:ci -c jest.config.$test.json
-docker compose -f docker-compose.$stack.yml down
+docker compose -f docker-compose.db.mariadb.yml run --rm --quiet-pull node
+docker compose -f docker-compose.db.mariadb.yml down
