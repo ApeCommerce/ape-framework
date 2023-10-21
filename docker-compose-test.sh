d9fdc8c695c2590a721dbf9ab6bbd8ffa0831d77
Matthieu Symoens <matthieu.symoens@gmail.com>
2023-10-21 16:21:22 -0400

    Integration test setup

--- docker-compose-test.sh
+++
@@ -1,2 +0,0 @@
-docker compose -f docker-compose.db.mysql.yml run --rm --quiet-pull node
-docker compose -f docker-compose.db.mysql.yml down -v -t0
---
+++ integration-test.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+test () {
+  file=test/stack/docker-compose.$1.yml
+
+  printf "\n================================================================\n"
+  printf "TEST $file"
+  printf "\n================================================================\n"
+
+  docker compose -f $file run --quiet-pull --remove-orphans --rm node
+
+  status=$?
+
+  docker compose -f $file down --volumes --timeout 0
+
+  if [ $status != 0 ]; then
+    exit 1
+  fi
+
+  sleep 1
+}
+
+test db.memory
+test db.mysql
+test db.sqlite
