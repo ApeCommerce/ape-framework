6377b136ba579af6d3469cfe659d66950fd7bb71
Matthieu Symoens <matthieu.symoens@gmail.com>
2023-12-03 10:17:06 -0500

    OpenAPI types

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,8 @@
+import { OpenAPI } from 'openapi-types'
+
 export interface Api {
   start: () => Promise<void>,
   close: () => Promise<void>,
   getUrl: () => string,
-  doc: () => unknown,
+  doc: () => OpenAPI.Document,
 }
