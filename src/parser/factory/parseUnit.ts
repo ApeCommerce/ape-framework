c3bda597b7db9835d366b957d6a3c8a6df07e261
Matthieu Symoens <matthieu.symoens@gmail.com>
2024-01-27 21:19:19 -0500

    Parser

---
+++ src/parser/factory/parseUnit.ts
@@ -0,0 +1,43 @@
+import { ParserInputError } from '../error/ParserInputError'
+import { parseNumber } from '../parseNumber'
+import { parseString } from '../parseString'
+import type { Parser } from '../Parser'
+
+interface Unit {
+  symbol: string,
+  value: number,
+}
+
+const parseUnit = (type: string, units: Unit[]): Parser<number> => {
+  return (input) => {
+    const allUnits: Unit[] = []
+    units.forEach((u) => {
+      allUnits.push(u, { ...u, symbol: ` ${u.symbol}` })
+    })
+    allUnits.sort((a, b) => {
+      return b.symbol.length - a.symbol.length
+    })
+    let string: string
+    try {
+      string = parseString(input)
+    } catch (error) {
+      throw new ParserInputError(type)
+    }
+    const unit = allUnits.find((u) => {
+      return string.toUpperCase().endsWith(u.symbol.toUpperCase())
+    })
+    let number: number
+    try {
+      number = parseNumber(
+        string.substring(0, string.length - (unit?.symbol.length || 0)),
+      ) * (unit?.value || 1)
+    } catch (error) {
+      throw new ParserInputError(type)
+    }
+    return number
+  }
+}
+
+export {
+  parseUnit,
+}
--- src/parser/parseMilliseconds.ts
+++ src/parser/parseMilliseconds.ts
@@ -1,14 +1,6 @@
-import { ParserInputError } from './error/ParserInputError'
-import { parseNumber } from './parseNumber'
-import { parseString } from './parseString'
-import type { Parser } from './Parser'
+import { parseUnit } from './factory/parseUnit'
 
-interface Unit {
-  symbol: string,
-  value: number,
-}
-
-const units: Unit[] = [
+const parseMilliseconds = parseUnit('milliseconds', [
   {
     symbol: 'ms',
     value: 1,
@@ -29,35 +21,7 @@ const units: Unit[] = [
     symbol: 'd',
     value: 86400000,
   },
-]
-
-const parseMilliseconds: Parser<number> = (input) => {
-  const allUnits: Unit[] = []
-  units.forEach((u) => {
-    allUnits.push(u, { ...u, symbol: ` ${u.symbol}` })
-  })
-  allUnits.sort((a, b) => {
-    return b.symbol.length - a.symbol.length
-  })
-  let string: string
-  try {
-    string = parseString(input)
-  } catch (error) {
-    throw new ParserInputError('milliseconds')
-  }
-  const unit = allUnits.find((u) => {
-    return string.toUpperCase().endsWith(u.symbol.toUpperCase())
-  })
-  let number: number
-  try {
-    number = parseNumber(
-      string.substring(0, string.length - (unit?.symbol.length || 0)),
-    ) * (unit?.value || 1)
-  } catch (error) {
-    throw new ParserInputError('milliseconds')
-  }
-  return number
-}
+])
 
 export {
   parseMilliseconds,
